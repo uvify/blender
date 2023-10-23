31b1a853a6efb8deac7f91d59e60dea26188d133
Sybren A. Stüvel <sybren@blender.org>
2023-10-23 11:35:11 +0200

    Anim: add temporary Animation Layers panel
    Add a temporary Animation Layers panel to the 3D Viewport. It's just there
    for some demos at the conference, so that the existence of the new
    animation data model can be seen *somewhere*.

---
+++ scripts/startup/bl_ui/anim_layers.py
@@ -0,0 +1,63 @@
+# SPDX-FileCopyrightText: 2023 Blender Authors
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+"""NOTE: this is temporary UI code to show animation layers.
+
+It is not meant for any particular use, just to have *something* in the UI.
+"""
+
+import bpy
+from bpy.types import Context, Panel
+
+
+class VIEW3D_PT_animation_layers(Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Animation"
+    bl_label = "Layers"
+
+    @classmethod
+    def poll(cls, context: Context) -> bool:
+        return context.object and context.object.animation_data
+
+    def draw(self, context: Context) -> None:
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False
+
+        adt = context.object.animation_data
+        if not adt:
+            return
+
+        col = layout.column()
+        col.prop(adt, 'animation')
+        col.prop(adt, 'animation_output_index', text="Output")
+
+        layout.separator()
+
+        anim = adt.animation
+        if not anim:
+            layout.label(text="No layers")
+            return
+
+        for layer_idx, layer in reversed(list(enumerate(anim.layers))):
+            layerbox = layout.box()
+            col = layerbox.column(align=True)
+            col.prop(layer, "name", text=f"Layer {layer_idx+1}:")
+
+            for strip_idx, strip in enumerate(layer.strips):
+                stripcol = col.column(align=True)
+                stripcol.label(text=f"Strip {strip_idx+1}:")
+                stripcol.prop(strip, "frame_start")
+                stripcol.prop(strip, "frame_end")
+                stripcol.prop(strip, "frame_offset")
+
+
+classes = (
+    VIEW3D_PT_animation_layers,
+)
+
+if __name__ == "__main__":  # only for live edit.
+    register, _ = bpy.utils.register_classes_factory(classes)
+    register()
